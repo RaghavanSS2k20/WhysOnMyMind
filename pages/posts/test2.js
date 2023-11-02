d4fbcc69e0742fd6aaf2eeaceca448053edca627
Raghavan M <rn75creatography@gmail.com>
2023-11-02 19:16:02 +0530

    skjfskjdfnskjdf

--- pages/posts/test2.js
+++ pages/posts/test2.js
@@ -1,25 +1,40 @@
-import { useState } from "react";
-const testCookies = ({ckies})=>{
-  const [cookies, setCookies] = useState(ckies.cookies)
- 
+import { useState , useEffect} from "react";
+const testCookies = ()=>{
+  const [cookies, setCookies] = useState(null)
+  useEffect(()=>{
+    const petch = async ()=>{
+    const res = await fetch('https://whyonm-api.onrender.com/api/cookies',{
+      credentials:'include',
+      // headers:{
+      //   Cookie: req.headers.cookie,
+      // }
+    })
+    const cokies = await res.json()
+    console.log(cokies.cookies)
+    setCookies(cokies.cookies)
+  }
+petch()
+  },[])
+  
+
   
   return(
     <h1>{cookies?cookies:'please wait'}</h1>
   )
 }
-export async function getServerSideProps(context){
-  const {req} = context
-  const res = await fetch('https://whyonm-api.onrender.com/api/cookies',{
-    credentials:'include',
-    headers:{
-      Cookie: req.headers.cookie,
-    }
-  })
-  const cokies = await res.json()
-  return{
-    props:{
-      ckies:cokies.cookies
-    }
-  }
-}
+// export async function getServerSideProps(context){
+//   const {req} = context
+//   const res = await fetch('https://whyonm-api.onrender.com/api/cookies',{
+//     credentials:'include',
+//     headers:{
+//       Cookie: req.headers.cookie,
+//     }
+//   })
+//   const cokies = await res.json()
+//   return{
+//     props:{
+//       ckies:cokies.cookies
+//     }
+//   }
+// }
 export default testCookies
